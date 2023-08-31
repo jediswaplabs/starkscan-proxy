2efad0c2ea553a45a569247d5e272ec7212cbe43
Prince Arora <77785932+princearoragithub@users.noreply.github.com>
2023-08-31 17:46:49 +0530

    path

--- proxy/__init__.py
+++ proxy/__init__.py
@@ -16,7 +16,7 @@ def proxy(path):
   contract_address = request.args.get('contract_address')
   owner_address = request.args.get('owner_address')
   
-  path = path.removeprefix("api/starkscan-proxy/")
+  path = path.removeprefix("api/")
 
   return get(f'{SITE_NAME}{path}', params = {"contract_address": contract_address, "owner_address": owner_address}, headers = {"X-API-KEY": API_KEY}).content
 
