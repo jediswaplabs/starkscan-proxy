ed88ecb195250618b71ca6d7e19626fcccb682c9
Prince Arora <77785932+princearoragithub@users.noreply.github.com>
2024-01-30 17:41:24 +0530

    transparent proxy

--- proxy/__init__.py
+++ proxy/__init__.py
@@ -6,19 +6,15 @@ app = Flask(__name__)
 SITE_ENV = os.environ.get('SITE_ENV', None)
 API_KEY = os.environ.get('API_KEY', None)
 if SITE_ENV == "testnet":
-  SITE_NAME = 'https://api-testnet.starkscan.co/api/v0/nfts/'
+  SITE_NAME = 'https://api-testnet.starkscan.co/'
 elif SITE_ENV == "mainnet":
-  SITE_NAME = 'https://api.starkscan.co/api/v0/nfts/'
+  SITE_NAME = 'https://api.starkscan.co/'
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def proxy(path):
-  contract_address = request.args.get('contract_address')
-  owner_address = request.args.get('owner_address')
-  
-  path = path.removeprefix("api/")
 
-  return get(f'{SITE_NAME}{path}', params = {"contract_address": contract_address, "owner_address": owner_address}, headers = {"X-API-KEY": API_KEY}).content
+  return get(f'{SITE_NAME}{path}', params = request.args, headers = {"X-API-KEY": API_KEY}).content
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
